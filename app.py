bdc4b92523b0e5f34efa3e503af86bfbecddd919
htp84 <henric.sundberg@gmail.com>
2017-11-26 21:05:00 +0100

    changed connection

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
+from pymongo import MongoClient
 import pprint
 import requests
 import pandas as pd
 from pandas.io.json import json_normalize
-from src.secrets import DATABASE, COLLECTION, API_KEY
+from src.secrets import CONN_STR
 import json
 
 #http://docs.mlab.com/data-api/
-URL = f"https://api.mlab.com/api/1/databases/{DATABASE}/collections/{COLLECTION}?apiKey="
+
 
 
 def main():
-    x = requests.get(URL + API_KEY).json()
-    df = json_normalize(x)
+    client = MongoClient(CONN_STR)
+    db = client.heroku_dc8bxz64
+    entries = db.entries
+    all_data = list(entries.find())
+    #print(all_data)
+    #pprint.pprint(db.collection_names(include_system_collections=False))
+    df = json_normalize(all_data)
     df['mmol'] = df.sgv / 18 # mmol/l = mg/dl / 18
     df['date1'] = pd.to_datetime(df.dateString)
-   # df['date1'] = df.date1.dt.strftime('yyyy-mm-dd')
     print(df.head())
-    #df = pd.read_json(list(x), orient="index")
-    #pprint.pprint(x)
-    #print(df.head())
 
 
 
