c093d6e09e98fad96427a538d70a5360f4c904e8
htp84 <henric.sundberg@gmail.com>
2017-11-26 15:06:10 +0100

    added e.g. pipfile.lock

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import pprint
 import requests
+import pandas as pd
+from pandas.io.json import json_normalize
 from src.secrets import DATABASE, COLLECTION, API_KEY
+import json
 
 #http://docs.mlab.com/data-api/
 URL = f"https://api.mlab.com/api/1/databases/{DATABASE}/collections/{COLLECTION}?apiKey="
@@ -8,7 +11,14 @@ URL = f"https://api.mlab.com/api/1/databases/{DATABASE}/collections/{COLLECTION}
 
 def main():
     x = requests.get(URL + API_KEY).json()
-    pprint.pprint(x)
+    df = json_normalize(x)
+    df['mmol'] = df.sgv / 18 # mmol/l = mg/dl / 18
+    df['date1'] = pd.to_datetime(df.dateString)
+   # df['date1'] = df.date1.dt.strftime('yyyy-mm-dd')
+    print(df.head())
+    #df = pd.read_json(list(x), orient="index")
+    #pprint.pprint(x)
+    #print(df.head())
 
 
 
