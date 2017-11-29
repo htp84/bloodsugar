60404f272155501a376f99f389c1a36609f9b757
htp84 <henric.sundberg@gmail.com>
2017-11-29 22:41:22 +0100

    added dash

--- app.py
+++ app.py
@@ -1,15 +1,70 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table_experiments as dt
+import datetime
+import json
+import pandas as pd
+import numpy as np
+import plotly
 from src.data import data, bloodsugar_describe
 from src.secrets import CONN_STR
 
-def main():
+app = dash.Dash()
+app.scripts.config.serve_locally = True
 
-    df = data(CONN_STR)
+df = data(CONN_STR)
+dateparts = ['minute', 'hour', 'day', 'week', 'month', 'quarter', 'year']
+df1 = bloodsugar_describe(df, 'hour')
+#print(df1.head())
 
+app.layout = html.Div([
+    html.H4('Blodsocker'),
+    dcc.Dropdown(
+        id='dropdown-datepart',
+        options=[{'label': i, 'value': i}
+                  for i in dateparts],
+        value='week'
+    ),
+    dcc.Input(
+        id='input-startdate',
+        type='Date',
+        value=datetime.date.today() - datetime.timedelta(days=14)
+    ),
+    dt.DataTable(
+        rows = df1.to_dict('records'),
 
-    df1 = bloodsugar_describe(df, 'week')
+        columns = ['date1', 'mean', 'std', 'min', '25%', '50%', '75%', 'max', 'count'],
 
+        row_selectable=False,
+        filterable=True,
+        sortable=True,
+        selected_row_indices=[],
+        id='datatable-bloodsugar-describe',
+        min_height=1000,
+        min_width=1000
+    ),
+    html.Div(id='selected-indexes'),
+    dcc.Graph(
+        id='graph-bloodsugar-describe'
+    ),
+], className="container")
 
+@app.callback(
+    dash.dependencies.Output('datatable-bloodsugar-describe', 'rows'),
+    [dash.dependencies.Input('dropdown-datepart', 'value'),
+    dash.dependencies.Input('input-startdate', 'value')])
+def update_table(datepart, startdate):
+    print(startdate)
+    rows = bloodsugar_describe(df, datepart, startdate=str(startdate)).to_dict('records')
+    return rows
+
+app.css.append_css({
+    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
+})
 
 if __name__ == '__main__':
-    main()
+    app.run_server(debug=True)
+
 
--- src/data.py
+++ src/data.py
@@ -20,6 +20,8 @@ def data(CONN_STR):
     return df
 
 
+    
+
 def bloodsugar_describe(df: pd.DataFrame, datepart: str, **kwargs):
     """Creates a summary table for the bloodsugar values
     
@@ -31,7 +33,7 @@ def bloodsugar_describe(df: pd.DataFrame, datepart: str, **kwargs):
     Keyword arguments
     -----------------
     all_data : bool, if set to True the summary table include all data. if all_data,
-            start and/or end are given all_data overwrites start and end.               
+               start and/or end are given all_data overwrites start and end.               
     amount : int, set the amount of 
     time_unit : str,
     start : datetime,
@@ -44,27 +46,19 @@ def bloodsugar_describe(df: pd.DataFrame, datepart: str, **kwargs):
     x = pd.to_timedelta(1, 'h')
     df.date1 = df.date1 + x
     df.sort_values('date1', ascending=1, inplace=True)
+
     all_data = kwargs.pop('all_data', False)
     amount = kwargs.pop('amount', 14)
     time_unit = kwargs.pop('time_unit', 'd')
     delta = pd.to_timedelta(amount, time_unit)
         
-    start = kwargs.pop('start', df.date1.max() - delta)
-    end = kwargs.pop('stop', df.date1.max())
+    start = kwargs.pop('startdate', df.date1.max() - delta)
+    end = kwargs.pop('stopdate', df.date1.max())
     
     if not all_data:
         df = df[(df['date1'] > start) & (df['date1'] <= end)]
+    
+    df = df.groupby(getattr(df.date1.dt, datepart))['mmol'].describe().reset_index().round(2)
 
-    df2 = pd.DataFrame()
-    
-    df2['mean'] = df.groupby(getattr(df.date1.dt, datepart))['mmol'].mean()
-    df2['std'] = df.groupby(getattr(df.date1.dt, datepart))['mmol'].std()
-    df2['median'] = df.groupby(getattr(df.date1.dt, datepart))['mmol'].median()
-    df2['75%'] = df.groupby(getattr(df.date1.dt, datepart))['mmol'].quantile(0.75)
-    df2['25%'] = df.groupby(getattr(df.date1.dt, datepart))['mmol'].quantile(0.25)
-    df2['min'] = df.groupby(getattr(df.date1.dt, datepart))['mmol'].min()
-    df2['max'] = df.groupby(getattr(df.date1.dt, datepart))['mmol'].max()
-    df2['count'] = df.groupby(getattr(df.date1.dt, datepart))['mmol'].count()
-    
         
-    return df2.reset_index()
+    return df
