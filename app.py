4e709625b4432f11b913b7e8a6a5293b57b2a8fe
htp84 <henric.sundberg@gmail.com>
2018-01-11 06:13:08 +0100

    added custom title

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import dash
-
+import numpy as np
+from apps.src.secrets import CONN_STR
+from apps.src.data import data
+df = data(CONN_STR, latest=True)
 app = dash.Dash(__name__)
+app.title = str(np.round(df.at[0, 'mmol'],1))
 server = app.server
 app.config.suppress_callback_exceptions = True
